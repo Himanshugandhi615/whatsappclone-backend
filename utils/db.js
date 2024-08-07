e2b018093255c86dc00fd76adbe34046be8f3c3b
Himanshugandhi <75466360+Himanshugandhi615@users.noreply.github.com>
2024-08-07 15:31:07 +0530

    link change

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect("mongodb+srv://gandhihimanshu58:wbfS9Fvrgocyl3un@cluster0.rtzvz.mongodb.net/whatsapp?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
     console.log("connection is successfull");
 }).catch((err)=>{
